2e6eba865706b4789f608c8b0868310f063657d1
NethmiPrabodhika <prabodhikanethmi@gmail.com>
2022-09-28 01:32:48 +0530

    cart modification

--- src/components/Products/ShoppingCart.js
+++ src/components/Products/ShoppingCart.js
@@ -5,57 +5,49 @@ import {
     Button,
 } from "reactstrap";
 import '../Products/style.css'
-import'../Products/style'
+import '../Products/style'
 import Swal from 'sweetalert2';
-
+import { Link, useNavigate } from "react-router-dom";
 
 const MyShoppingCart = (props) => {
 
-  //shopping cart
-  const [CartData, setCartData] = useState([]);
-  const [totalPrice, settotalPrice] = useState(0);
+    //shopping cart
+    const [CartData, setCartData] = useState([]);
+    const [totalPrice, settotalPrice] = useState(0);
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    console.log(props);
-    setCartData(props.data);
-  }, [])
+    useEffect(() => {
+        console.log(props);
+        setCartData(props.data);
+    }, [])
 
 
 
-  const Payment = (e) => {
+    const Payment = (e) => {
+       
+        e.preventDefault();
+        navigate("/payment");
+    }
 
-  }
-  
+    return (
+        <div className='container' style={{ width: '960px' }}>
+            <br></br>
+            <br></br>
+            <center>
+                <div class="card" >
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h4><b>MY SHOPPING CART</b></h4>
+                        </div>
+                    </div>
+                </div>
 
-  return (
-    <div className='container'>
-        <br></br>
-        <br></br>
-        <center>
-        <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center">
-            <h3 class="animate-charcter"><b>My Shopping Cart</b></h3>
-            </div>
-        </div>
-        </div>
+            </center>
 
-        </center>
-        <br></br>
-        <br/><br/>
-
-        <div>
-            <Button 
-                className="btn btn-dark" style={{fontSize: "16px", float:'right',width:'200px'}} 
-                onClick={(e)=>Payment(e)}
-                > 
-                Continue to Payment
-            </Button>
-        </div>
-        <br/><br/><br/>
-        <div>
-            <table className="table table-striped table-bordered">
-                {/* <thead>
+            <br /><br />
+            <div>
+                <table className="table table-striped table-bordered">
+                    {/* <thead>
                     <tr>
                         <th>Product Name</th>
                         <th>Product Price</th>
@@ -75,52 +67,65 @@ const MyShoppingCart = (props) => {
                         )
                     })}
                 </tbody> */}
-                <thead>
-                    <tr>
-                        <th>Product Name</th>
-                        <th>Product Price</th>
-                    </tr>
-                </thead>
-                <tbody> 
-                    {CartData.map((item, index) => {
-                        return (    
-                            <tr key={index}>
-                                <td>{item.productName}</td>
-                                <td>{item.productPrice}</td>
-                            </tr>
-                        )
-                    })}
-                    <tr>
-                        <td><b>Total Price</b></td>
-                        <td>{localStorage.getItem("totalPrice")}</td>
-                    </tr>
-                </tbody> 
-            </table>
-        </div>
-        <br></br>
-        <br/><br/>
+                    <thead>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>Product Price (LKR)</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {CartData.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.productName}</td>
+                                    <td>{item.productPrice}.00</td>
+                                </tr>
+                            )
+                        })}
+                        <tr>
+                            <td><b>Total Price (LKR)</b></td>
+                            <td>{localStorage.getItem("totalPrice")}.00</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
 
+            <div>
+
+            </div>
+            {/* class="animate-charcter" */}
             <div class="col-md-12 text-center">
-                <b class="animate-charcter"><b>My Items</b></b>
+                {/* <b>Items in my cart</b> */}
             </div>
             <section class="cards" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', marginTop: '30px' }}>
 
                 {CartData.map((product) => (
-                <article class="card" style={{ flex: '0 1 24%', borderWidth: '1px',borderColor:'white', marginBottom: '20px' }}>
-                    <img src={product.productImage} alt='No Image Added...' style={{ width: '100%', height: 'auto' }} />
-                    <h6>{product.productName}</h6>
-                    <p><b>LKR. {product.productPrice}</b></p>
-                </article>
+                    <article class="card" style={{ flex: '0 1 24%', borderWidth: '1px', borderColor: 'white', marginBottom: '20px' }}>
+                        <img src={product.productImage} alt='No Image Added...' style={{ width: '50%', height: 'auto' }} />
+                        <h6>{product.productName}</h6>
+                        <p><b>LKR. {product.productPrice}</b></p>
+                    </article>
                 ))}
 
 
             </section>
 
-        
+            <center>
+                <Button
+                    className="btn btn-dark" style={{ fontSize: "16px", width: '220px', height: '40px', marginBottom: '40px' }}
+                    onClick={(e) => Payment(e)}
+                >
+                    Continue to Payment
+                    &nbsp;
+
+                    <i class="fa-solid fa-money-check-dollar"></i>
+                </Button>
+
+            </center>
 
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default MyShoppingCart;
